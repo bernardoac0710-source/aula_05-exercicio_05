ac4787e3a6de598e103fa42697d63ad7cdf03de8
Bernardo <you@example.com>
2026-03-25 11:31:00 -0300

    exercicio05

---
+++ src/App.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner entrada = new Scanner(System.in);
+        double soma = 0;
+        while (true) {
+
+            System.out.print("digite um número para a soma( digite 0 para encerrar): ");
+            double numero = entrada.nextDouble();
+
+            if (numero == 0) {
+                break;
+            }
+            soma += numero;
+
+        }
+
+        System.out.print("A soma total é: " + soma);
+        entrada.close();
+    }
+}
